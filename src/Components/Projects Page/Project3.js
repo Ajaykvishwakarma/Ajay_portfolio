4acceb4aa02580ceaddf5de33cbaa72a066211b4
Ajaykvishwakarma <ajaykv054@gmail.com>
2022-05-07 19:53:28 +0530

    final commit

--- src/Components/Projects Page/Project3.js
+++ src/Components/Projects Page/Project3.js
@@ -10,8 +10,8 @@ function Project3() {
       <div className="project__main">
         <img
           className="project__image"
-          src="https://raw.githubusercontent.com/Ajaykvishwakarma/Portfolio/main/src/landing_page2.png"
-          srcset="https://raw.githubusercontent.com/Ajaykvishwakarma/Portfolio/main/src/landing_page2.png 750w, https://raw.githubusercontent.com/Ajaykvishwakarma/Portfolio/main/src/landing_page2.png 1468w"
+          src="https://raw.githubusercontent.com/Ajaykvishwakarma/Images/main/twitter_starting.png"
+          srcset="https://raw.githubusercontent.com/Ajaykvishwakarma/Images/main/twitter_starting.png 750w, https://raw.githubusercontent.com/Ajaykvishwakarma/Images/main/twitter_starting.png 1468w"
           sizes="50vw"
           alt="Lavi's Landing Page"
           width="50%"
@@ -19,22 +19,21 @@ function Project3() {
           loading="lazy"
         />
         <div data-aos="fade-left" className="project__right">
-         <a href = "https://levi-s-in.vercel.app/" target="_blank" rel="noopener noreferrer" className="prod_Anchor"> <h3 className="project__title" id="title">Levi's Clone </h3></a>
+         <a href = "https://twitter-clone-ba49b.web.app/" target="_blank" rel="noopener noreferrer" className="prod_Anchor"> <h3 className="project__title" id="title">Twitter's Clone </h3></a>
           <p className="project__description project__body">
-            A <span className="project__specialText">Levi.in a clone. </span>The project
-            for E-Commerce website 2022. Allows users to{" "}
-            <span className="project__specialText">Buy Clothing items.</span> Store locator{" "}
-            <span className="project__specialText">that locates Levi's store in India.</span>
+            A <span className="project__specialText">Twitter clone. </span>This Project is about the clone of Twitter Website, a Social Media, Where People can share Thoughts, Post Messages.{" "}
+            <span className="project__specialText">I Made This Project using React, </span> Material Ui{" "}
+            <span className="project__specialText">and also used 🔥Firebase as database and also deployed on Firebase.</span>
             {" "}
             {/* <span className="project__specialText">Spoonacular API</span>. */}
           </p>
           <p className="project__tech project__body">
-            &gt; HTML · CSS · JavaScript · Localhost · Google Map API
+            &gt; HTML · CSS · JavaScript · React · Meterial Ui . Firebase . Bootstrap
           </p>
           <div className="linksDiv">
           <a
             className="project__sourceCode"
-            href="https://levi-s-in.vercel.app/"
+            href="https://twitter-clone-ba49b.web.app/"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="=Website GitHub Repository"
@@ -43,7 +42,7 @@ function Project3() {
           </a>
           <a
             className="project__sourceCode"
-            href="https://github.com/Ajaykvishwakarma/levi-s.in"
+            href="https://github.com/Ajaykvishwakarma/Twitter"
             target="_blank"
             rel="noopener noreferrer"
             aria-label="=Website GitHub Repository"
--- src/Components/Projects Page/Projects.js
+++ src/Components/Projects Page/Projects.js
@@ -2,6 +2,7 @@ import Project1 from "./Project1";
 import Project2 from "./Project2";
 import Project3 from "./Project3";
 
+
 function Projects() {
   return (
     <section className="projects">
